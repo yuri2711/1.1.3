586105a0ad644d6848fce8a539b698eebfd0c407
yuri2711 <39201286+yuri2711@users.noreply.github.com>
2021-04-07 08:36:47 +0300

    Ок

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService service = new UserServiceImpl();
+        service.createUsersTable();
+        //service.dropUsersTable();
+        service.saveUser("Юрий", "Тарасенко", (byte) 35);
+        service.saveUser("Иван", "Ирзин", (byte) 16);
+        service.saveUser("Сергей", "Облаухов", (byte) 35);
+        service.saveUser("Антон", "Царев", (byte) 29);
+        service.saveUser("Юрий", "Герасимов", (byte) 65);
+        service.removeUserById(2);
+        for (User user : service.getAllUsers()) System.out.println(user);
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,18 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String url = "jdbc:mysql://localhost:3306/sakila?autoReconnect=true&useSSL=false";
+    private static final String user = "root";
+    private static final String pass = "7777";
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException{
+            connection = DriverManager.getConnection(url, user,pass);
+        return connection;
+    }
 }
